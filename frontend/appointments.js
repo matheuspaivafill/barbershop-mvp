3ec22d86930d610bf8f508db50cb7a6f2d2da989
matheuspaivafill <c.teuspaiva13@gmail.com>
2026-06-09 00:40:46 -0300

    Implementa CRUD completo de agendamentos no frontend

--- frontend/appointments.js
+++ frontend/appointments.js
@@ -4,6 +4,7 @@ const button = document.getElementById("register-button")
 const clientId = document.getElementById("client_id")
 const date = document.getElementById("date")
 const time = document.getElementById("time")
+const appointmentsList = document.getElementById("appointments-list")
 
 
 button.addEventListener("click", function (event) {
@@ -25,9 +26,16 @@ button.addEventListener("click", function (event) {
     
     .then(data => {
 
-    console.log(data)
+        console.log(data)
+
+        if (data.Error) {
+            alert(data.Error)
+            return
+        }
 
     alert("Agendamento criado!")
+
+    loadAppointments()
 
     clientId.value = ""
     date.value = ""
@@ -38,7 +46,77 @@ button.addEventListener("click", function (event) {
     .catch(error => {
 
     console.log(error)
+
+    alert("Erro ao criar agendamento!")
 })
 
 })
 
+function loadAppointments() {
+    appointmentsList.innerHTML = ""
+
+    fetch("http://127.0.0.1:8000/appointments")
+
+        .then(response => response.json())
+
+        .then(data => {
+
+            console.log(data)
+
+            data.forEach(function(appointment) {
+
+            const appointmentCard = document.createElement("div")
+            const deleteButton = document.createElement("button")
+
+            deleteButton.innerText = "🗑"
+
+            appointmentCard.classList.add("client-card")
+
+            
+
+            appointmentCard.innerText =
+            `Cliente ID: ${appointment.client_id}
+            | Data: ${appointment.date}
+            | Horário: ${appointment.time}`
+            
+            appointmentCard.appendChild(deleteButton)
+
+            deleteButton.addEventListener("click", function() {
+
+                fetch(
+                    `http://127.0.0.1:8000/appointment/${appointment.id}`,
+                    {
+                    method: "DELETE"
+                     }
+               )
+
+               .then(response => response.json())
+
+               .then(data => {
+
+                    console.log(data)
+
+                    loadAppointments()
+
+              })
+
+               .catch(error => {
+
+                    console.log(error)
+
+             })
+            
+       })
+        appointmentsList.appendChild(appointmentCard)
+    })
+
+})
+
+.catch(error => {
+
+    console.log(error)
+
+})
+
+}
+loadAppointments()
